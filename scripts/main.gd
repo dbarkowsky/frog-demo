b83272cbdb0978f453675f1733f61a2729163ba3
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2026-09-19 10:15:38 -0700

    populate 3-frog layout

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,15 @@
 extends Control
 
 const FROG_TEMPLATE = preload("res://frog_page.tscn")
-@onready var scroll: ScrollContainer = $ScrollContainer
-@onready var container: HBoxContainer = $ScrollContainer/HBoxContainer
 
 var swipe_start: Vector2 = Vector2.ZERO
 var minimum_drag: float = 100.0
+
+var frog_list: Array
+# For all the following arrays,
+# it's intended that 0 = left, 1 = centre, 2 = right
+var instanceArray: Array
+var positionArray: Array
 	
 func _input(event: InputEvent) -> void:
 		if event is InputEventScreenTouch:
@@ -16,34 +20,43 @@ func _input(event: InputEvent) -> void:
 				if swipe_vector.length() >= minimum_drag:
 					if abs(swipe_vector.x) > abs(swipe_vector.y):
 						var tween = create_tween()
+						tween.set_parallel()
 						tween.set_trans(Tween.TRANS_QUAD)
 						tween.set_ease(Tween.EASE_OUT)
 						if swipe_vector.x > 0:
 							print("Swiped Right")
-							tween.tween_property(scroll, "scroll_horizontal", 0, 0.5)
+							#tween.tween_property(scroll, "scroll_horizontal", 0, 0.5)
 						else:
 							print("Swiped Left")
-							tween.tween_property(scroll, "scroll_horizontal", 450, 0.5)
+							#tween.tween_property(scroll, "scroll_horizontal", 450, 0.5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for child in container.get_children():
-		child.queue_free()
+	# Set up positions based on screen size
+	var screen_width = get_viewport_rect().size.x
+	positionArray.append(Vector2(-screen_width * 1.5, 0)) # Left
+	positionArray.append(Vector2(0, 0)) # Centre
+	positionArray.append(Vector2(screen_width * 1.5, 0)) # Right
+	
 	# Load the json data that defines frog cards
-	var frog_list: Array = load_json_array("res://data/frog_data.json")
-	container.add_theme_constant_override("separation", 0)
+	frog_list = load_json_array("res://data/frog_data.json")
 	# Loop directly through each dictionary inside the JSON array
-	for frog_data in frog_list:
+	var positionIndex = 0
+	var frog_index = frog_list.size() - 1 # left is last frog in data
+	for i in range(3):
+		var frog_data = frog_list[frog_index]
+		frog_index = (frog_index + 1) % frog_list.size()
 		if typeof(frog_data) == TYPE_DICTIONARY:
 			# 1. Create a copy of the template
 			var new_frog = FROG_TEMPLATE.instantiate()
-			#var screen_size = get_viewport_rect().size
-			#new_frog.custom_minimum_size = screen_size
+			new_frog.global_position = positionArray[positionIndex]
+			positionIndex += 1
 			# 2. Add it to the tree so its @onready nodes initialize
-			container.add_child(new_frog)
+			add_child(new_frog)
 			
 			# 3. Send the dictionary data into the template
 			new_frog.hydrate(frog_data)
+			instanceArray.append(new_frog)
 
 func load_json_array(file_path: String) -> Array:
 	if not FileAccess.file_exists(file_path):
