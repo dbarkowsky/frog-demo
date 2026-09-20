a0d63d89bb1472a6bf6036a687eeccd608faf79e
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2026-09-19 17:24:06 -0700

    click animation icon, add waveform images

--- scripts/frog_page.gd
+++ scripts/frog_page.gd
@@ -3,6 +3,9 @@ extends Control
 @onready var sound: AudioStreamPlayer = $FrogSound
 @onready var image: TextureButton = $FrogPic
 @onready var title: RichTextLabel = $FrogText
+@onready var latin: RichTextLabel = $FrogLatin
+@onready var click: Sprite2D = $Click
+@onready var wave: TextureRect = $Waveform
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +22,9 @@ func hydrate(frog: Dictionary) -> void:
 	sound.stream = load(frog.get("audio"))
 	image.texture_normal = load(frog.get("img"))
 	title.text = frog.get("name")
+	latin.text = frog.get("latin")
+	print(frog.get("wave"))
+	wave.texture = load(frog.get("wave")) as Texture2D
 	
 func stop_audio() -> void:
 	if sound and sound.playing:
