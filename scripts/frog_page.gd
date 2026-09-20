7cb29825a86b22c8ae1558bd2c56881326ee75ec
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2026-09-19 21:28:03 -0700

    asset changes, audio wave bar, bug fixes

--- scripts/frog_page.gd
+++ scripts/frog_page.gd
@@ -6,6 +6,9 @@ extends Control
 @onready var latin: RichTextLabel = $FrogLatin
 @onready var click: Sprite2D = $Click
 @onready var wave: TextureRect = $Waveform
+@onready var stick: Sprite2D = $Waveform/Stick
+
+var tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +17,11 @@ func _ready() -> void:
 	
 func _on_frog_pic_pressed() -> void:
 	if sound.stream:
-		sound.play()
+		if not sound.playing:
+			start_audio()
+		else:
+			stop_audio()
+			start_audio() # just reset basically
 	else:
 		print("No sound stream loaded for this frog!")
 
@@ -23,9 +30,30 @@ func hydrate(frog: Dictionary) -> void:
 	image.texture_normal = load(frog.get("img"))
 	title.text = frog.get("name")
 	latin.text = frog.get("latin")
-	print(frog.get("wave"))
 	wave.texture = load(frog.get("wave")) as Texture2D
-	
+
+func start_audio() -> void:
+	tween = create_tween()
+	# End value depends on where end of audio wave is.
+	# Time to get there depends on length of audio...
+	var startX = wave.position.x
+	var endX = startX + wave.size.x
+	var length = sound.stream.get_length()
+	var half_width = stick.texture.get_width() * stick.scale.x * 2
+	startX -= half_width
+	endX -= half_width
+	# Set the stick to the start in case it isn't there
+	stick.position.x = startX
+	# And move stick to end
+	tween.tween_property(stick, "position:x", endX, length)
+	sound.play()
+			
 func stop_audio() -> void:
 	if sound and sound.playing:
 		sound.stop()
+	if tween and tween.is_valid():
+		tween.kill()
+		var startX = wave.position.x
+		var half_width = stick.texture.get_width() * stick.scale.x * 2
+		startX -= half_width
+		stick.position.x = startX	
